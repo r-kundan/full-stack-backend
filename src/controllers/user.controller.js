af1d58fd2bcada6f398e997be47efea9f1e1e390
apple <apple@apples-MacBook-Pro.local>
2024-02-02 22:44:17 +0530

    login and logout part add

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,33 @@
 import { asyncHandler } from "../utiliti/asyncHandler.js"
 import { ApiError } from "../utiliti/apiError.js"
-import { User } from "../models/user.model.js"
+import {User} from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utiliti/cloudinary.js"
 import { ApiResponse } from "../utiliti/apiRespose.js"
+// import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
+
+
+
+
+const generateAccessAndRefreshTokens = async(userId) =>{
+    try {
+      //yha ye user ko find kr rha h 
+        const user = await User.findById(userId)
+ // access and refresh token generate kr rhe h
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+    //refresh token save in db
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+//return tokens
+        return {accessToken, refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
+
 
 const registerUser = asyncHandler(async (req, res) => {
     // get user details from frontend
@@ -28,9 +53,8 @@ const registerUser = asyncHandler(async (req, res) => {
     // throw new ApiError(400,"fullname is required")
     //     }
     if (
-        [fullName, email, userName, password].some((field) => field?.trim() === "")
-
-    ) {
+        [fullName, email, userName, password].some((field) => field?.trim() === ""))
+     {
         throw new ApiError(400, "fullname is required")
     }
     //check if user already exists: username , email--------------------
@@ -39,6 +63,7 @@ const registerUser = asyncHandler(async (req, res) => {
         //use oprator to check one is exists: username or email 
         $or: [{ userName }, { email }]
     })
+
     if (existedUser) {
         throw new ApiError(409, "user email or userName already exists")
     }
@@ -88,4 +113,82 @@ const registerUser = asyncHandler(async (req, res) => {
     )
 })
 
-export { registerUser }
+
+const loginUser = asyncHandler(async (req,res) => {
+    //req body  =   data
+    //username or email
+    //find user
+    //password check
+    //access and referesh token
+    //send cookies
+    const { userName, email, password } = req.body
+
+    if (!(userName  || email)){
+        throw new ApiError(404, "userName and email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{ userName }, { email }]
+    })
+    if(!user){
+        throw new ApiError(404,"user not found")
+    }
+
+const isPasswordVaild = await user.isPasswordCorrect(password)
+    if(!isPasswordVaild){
+        throw new ApiError(401,"password is not correct")
+    }
+    //access and referesh token
+
+  const {refreshToken,accessToken} = await generateAccessAndRefreshTokens(user._id)
+
+  const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+  
+  //send cookies
+  const options ={
+    httpOnly : true,
+    secure: true
+  }
+  return  res
+  .status(200)
+  .cookie("accessToken",accessToken,options)
+  .cookie("refreshToken",refreshToken,options)
+  .json(
+    new ApiResponse(200,
+        {
+            user:loggedInUser,accessToken,refreshToken
+        },
+        "user logged in sucessfully")
+  )
+
+})
+
+const logOutUser = asyncHandler(async(req,res)=>{
+   await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken:  undefined
+        }
+    },
+    {
+        new: true
+    }
+    )
+    const options ={
+        httpOnly : true,
+        secure: true
+      }
+      return  res
+      .status(200)
+  .clearCookie("accessToken",options)
+  .clearCookie("refreshToken",options)
+  .json(
+    new ApiResponse(200,
+        {},
+        "user logout  sucessfully")
+  )
+
+})
+    
+export { registerUser,loginUser ,logOutUser}
